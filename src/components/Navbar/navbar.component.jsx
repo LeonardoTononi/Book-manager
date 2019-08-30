a92646a6a1ad841d2682968a8da47e46e8920061
Leonardo Tononi <easaaa@protonmail.com>
2019-08-30 12:08:21 +0200

    Add Link to navbar

--- src/components/Navbar/navbar.component.jsx
+++ src/components/Navbar/navbar.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 import './navbar.styles.scss';
 
@@ -7,9 +8,8 @@ const Navbar = () => {
     <div className="container-nav">
       <h1 className="logo">Book Manager</h1>
       <ul className="navbar">
-        <li>Home</li>
-        <li>Add Book</li>
-        <li>Book List</li>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/books-list">Book List</Link></li>
         <li className="login">LogIn</li>
         <li className="signup">SignUp</li>
         <li className="logout">LogOut</li>
