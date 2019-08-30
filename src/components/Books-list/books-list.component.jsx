2d3c8b612920627513518304a56bcf016feff7c6
Leonardo Tononi <easaaa@protonmail.com>
2019-08-30 15:31:13 +0200

    Refactored book-list component

--- src/components/Books-list/books-list.component.jsx
+++ src/components/Books-list/books-list.component.jsx
@@ -3,6 +3,22 @@ import React from 'react';
 import './books-list.styles.scss'
 
 const BooksList = ({ books, deleteBook }) => {
+  
+  const books_th = books.map(book => {
+    return (
+      <tr key={book.id}>
+        <th>{book.title}</th>
+        <th>{book.author}</th>
+        <th>{book.note}</th>
+        <th>
+          <button onClick={() => {
+            deleteConfirm(book.id)
+          }}>X</button>
+        </th>
+      </tr>
+    )
+  })
+
   const deleteConfirm = (bookID) => {
     if (window.confirm('Are you sure?')) {
       deleteBook(bookID);
@@ -14,27 +30,16 @@ const BooksList = ({ books, deleteBook }) => {
     return (
       <div className="list-container" >
         <h2>Book List</h2>
-        <tbody>
-          <tr className="table-title">
-            <th>Title</th>
-            <th>Author</th>
-            <th>Note</th>
-          </tr>
-          {books.map(book => {
-            return (
-              <tr key={book.id}>
-                <th>{book.title}</th>
-                <th>{book.author}</th>
-                <th>{book.note}</th>
-                <th>
-                  <button onClick={() => {
-                    deleteConfirm(book.id)
-                  }}>X</button>
-                </th>
-              </tr>
-            )
-          })}
-        </tbody>
+        <table>
+          <tbody>
+            <tr className="table-title">
+              <th>Title</th>
+              <th>Author</th>
+              <th>Note</th>
+            </tr>
+            {books_th}
+          </tbody>
+        </table>
       </div>
     )
   }
