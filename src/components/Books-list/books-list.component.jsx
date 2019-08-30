3ed683fb3adcaf35ca605fcc391095278d4aaab9
Leonardo Tononi <easaaa@protonmail.com>
2019-08-30 12:09:07 +0200

    Added delete confirmation to book list

--- src/components/Books-list/books-list.component.jsx
+++ src/components/Books-list/books-list.component.jsx
@@ -2,33 +2,41 @@ import React from 'react';
 
 import './books-list.styles.scss'
 
-const BooksList = ({books, deleteBook}) => {
-  const bookRow = books.map(book => {
-    return (
-      <tr key={book.id}>
-        <th>{book.title}</th>
-        <th>{book.author}</th>
-        <th>{book.note}</th>
-        <th>
-          <button onClick={() => deleteBook(book.id)}>X</button>
-        </th>
-      </tr>
-    )
-  })
+const BooksList = ({ books, deleteBook }) => {
+  const deleteConfirm = (bookID) => {
+    if (window.confirm('Are you sure?')) {
+      deleteBook(bookID);
+    } else {
+      return
+    }
+  }
 
-  return (
-    <div className="list-container">
-      <h2>Book List</h2>
-      <tbody>
-        <tr className="table-title">
-          <th>Title</th>
-          <th>Author</th>
-          <th>Note</th>
-        </tr>
-        {bookRow}
-      </tbody>
-    </div>
-  )
-}
+    return (
+      <div className="list-container" >
+        <h2>Book List</h2>
+        <tbody>
+          <tr className="table-title">
+            <th>Title</th>
+            <th>Author</th>
+            <th>Note</th>
+          </tr>
+          {books.map(book => {
+            return (
+              <tr key={book.id}>
+                <th>{book.title}</th>
+                <th>{book.author}</th>
+                <th>{book.note}</th>
+                <th>
+                  <button onClick={() => {
+                    deleteConfirm(book.id)
+                  }}>X</button>
+                </th>
+              </tr>
+            )
+          })}
+        </tbody>
+      </div>
+    )
+  }
 
 export default BooksList
