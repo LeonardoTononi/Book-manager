9a0b7bad64857c75bdfc66fbf903be184317620b
Leonardo Tononi <easaaa@protonmail.com>
2019-09-03 11:39:22 +0200

    Updates

---
+++ precache-manifest.5d5718114020683e3568c2d5a140f4a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5ea90436fd0f15ed8a979e529e5e7c8",
+    "url": "/Book-manager/index.html"
+  },
+  {
+    "revision": "b6480bb9fe9d0d896dcb",
+    "url": "/Book-manager/static/css/main.c1dbd08d.chunk.css"
+  },
+  {
+    "revision": "4e0c19bde654e2b3c897",
+    "url": "/Book-manager/static/js/2.45e6fe10.chunk.js"
+  },
+  {
+    "revision": "b6480bb9fe9d0d896dcb",
+    "url": "/Book-manager/static/js/main.860c2367.chunk.js"
+  },
+  {
+    "revision": "f453d8c785f8e66acb13",
+    "url": "/Book-manager/static/js/runtime~main.ad83e04f.js"
+  }
+]);
